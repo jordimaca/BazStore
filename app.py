9a147bf49df89955090f2498dfe977ac055bafe5
Daniel-Lama <josedaniel25pl@gmail.com>
2023-06-16 14:51:23 -0400

    Creacion de Articulo

--- app.py
+++ app.py
@@ -52,6 +52,9 @@ def vendedor():
 @app.route('/producto')
 def producto():
     return render_template('sitio/producto.html')
+@app.route('/articulo')
+def articulo():
+    return render_template('sitio/articulo.html')
 
     #Subcategorias
 @app.route('/accesorios/anillos')
