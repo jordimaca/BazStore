055fcc0abd45a9bba54392f89697187e0213b4c5
Jordi Carrasco <jordimaca13@hotmail.com>
2023-07-11 17:32:02 -0400

    Update bazstore.sql

--- bazstore.sql
+++ bazstore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-06-2023 a las 23:35:36
+-- Tiempo de generación: 11-07-2023 a las 23:28:37
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.2.13
 
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `bazstore`
+-- Base de datos: `bastore`
 --
 
 -- --------------------------------------------------------
@@ -32,7 +32,7 @@ CREATE TABLE `articulo` (
   `id_articulo` int(11) NOT NULL,
   `id_usuario` int(11) NOT NULL,
   `nombre_articulo` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
-  `imagen` mediumblob NOT NULL,
+  `imagen` varchar(255) COLLATE utf8_spanish_ci NOT NULL,
   `precio` int(7) NOT NULL,
   `tipo` char(13) COLLATE utf8_spanish_ci NOT NULL,
   `talla` varchar(5) COLLATE utf8_spanish_ci NOT NULL,
@@ -54,7 +54,7 @@ CREATE TABLE `usuario` (
   `email` varchar(320) COLLATE utf8_spanish_ci NOT NULL,
   `celular` varchar(10) COLLATE utf8_spanish_ci NOT NULL,
   `contraseña` varchar(60) COLLATE utf8_spanish_ci NOT NULL,
-  `imagen` mediumblob NOT NULL
+  `imagen` varchar(255) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -62,7 +62,7 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `email`, `celular`, `contraseña`, `imagen`) VALUES
-(1, 'admin', 'admin@admin.com', '8098000000', 'admin', 0x3c73766720786d6c6e733d22687474703a2f2f7777772e77332e6f72672f323030302f737667222077696474683d22323422206865696768743d223234222076696577426f783d22302030203234203234223e3c7061746820643d224d3132203243362e3537392032203220362e353739203220313273342e3537392031302031302031302031302d342e3537392031302d31305331372e343231203220313220327a6d30203563312e3732372030203320312e32373220332033732d312e32373320332d332033632d312e37323620302d332d312e3237322d332d3373312e3237342d3320332d337a6d2d352e31303620392e373732632e3839372d312e333220322e3339332d322e3220342e3130362d322e32683263312e373134203020332e3230392e383820342e31303620322e324331352e3832382031382e31342031342e303135203139203132203139732d332e3832382d2e38362d352e3130362d322e3232387a222f3e3c2f7376673e);
+(1, 'admin', 'admin@admin.com', '8098000000', 'admin', '<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\"><path d=\"M12 2C6.579 2 2 6.579 2 12s4.579 10 10 10 10-4.579 10-10S17.421 2 12 2zm0 5c1.727 0 3 1.272 3 3s-1.273 3-3 3c-1.726 0-3-1.272-3-3s1.274-3 3-3zm-5.106 9.772c.897-1.');
 
 --
 -- Índices para tablas volcadas
