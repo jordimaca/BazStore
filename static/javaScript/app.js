71e35f4af8b9dc118d2dc7b8cb526e8941552255
kimberly turbi <kimberlyturbi23@gmail.com>
2023-08-10 14:52:05 -0400

    btn-eliminar y ocultar el btn

--- static/javaScript/app.js
+++ static/javaScript/app.js
@@ -40,4 +40,15 @@ function bannerImg() {
 
 // Boton eliminar en perfil.html
 
+function Toggle() {
+  var x = document.getElementsByClassName("btn-eliminar");
+  for( let i = 0; i < x.length; i++){
+    if (x[i].style.display === "none") {
+      x[i].style.display = "block";
+    } else {
+      x[i].style.display = "none";
+    }
+  }
+    
+}
 // FIN
